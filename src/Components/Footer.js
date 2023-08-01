d2515886db8f3d85ea33833184004c4522c6a0d0
FaizWdr <fayiz.kappil@gmail.com>
2023-08-01 11:18:30 +0530

    Port-Link-Changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
       <div className="footer-right">
         <nav className="footer-navigation">
           <a href="https://pickled-brace-695.notion.site/Faiz-s-Blog-b35325a941e74dc9bec5558652db278b?pvs=4">Blogs</a>
-          <a href="https://pickled-brace-695.notion.site/Mohammed-Faiz-K-b2ddfc03a508464eb0f3751d0cf34c46?pvs=4">Portfolio</a>
+          <a href="https://bento.me/faizrahim">Portfolio</a>
         </nav>
         <button className="download-cv-button" onClick={handleDownloadCV}>Download CV</button>
       </div>
