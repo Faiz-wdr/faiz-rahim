2f8346a0fea937ad813d9a3f7902fdde0c915920
FaizWdr <fayiz.kappil@gmail.com>
2023-10-14 17:50:23 +0530

    Job updated

--- src/Components/data.js
+++ src/Components/data.js
@@ -2,9 +2,18 @@
 import { PiBriefcaseBold, PiBroomBold } from "react-icons/pi";
 
 export const Experience = [
+  {
+    id: 1,
+    date: "2023 Oct - present",
+    iconsSrc: <PiBriefcaseBold />,
+    title: "Creative Designer",
+    location: "Kozhikode, India",
+    description:
+      "As a Creative Designer, I lead the visual storytelling through graphic design and UI design for Shabzalio.",
+  },
     {
-      id: 1,
-      date: "2023 Jan - present",
+      id: 2,
+      date: "2023 Jan - 2023 Oct",
       iconsSrc: <PiBriefcaseBold />,
       title: "Graphic Designer",
       location: "Hyderabad, India",
@@ -12,7 +21,7 @@ export const Experience = [
         "As a Core Committee Member, I oversee 'The Change' graphic design team.",
     },
     {
-        id: 2,
+        id: 3,
         date: "2020 - 2023",
         iconsSrc: <PiBroomBold />,
         title: "UI & Graphic Design",
