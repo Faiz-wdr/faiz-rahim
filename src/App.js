00ae60f3d4e67a048ec355c5d8ade13805f9af40
FaizWdr <fayiz.kappil@gmail.com>
2023-12-09 13:25:50 +0530

    Analytics added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Router from './Router';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-257712253-1"; 
 
+ReactGA.initialize(TRACKING_ID);
 function App() {
   return (
     <Router />
