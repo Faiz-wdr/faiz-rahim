5a0d52380037e2ab3bea3384f6409573f4b19989
FaizWdr <fayiz.kappil@gmail.com>
2024-01-18 19:06:53 +0530

    projects updated

--- src/Components/ProjectSection.js
+++ src/Components/ProjectSection.js
@@ -10,9 +10,9 @@ const ProjectSection = () => {
         <h2>Recent Projects</h2>
         <div className="project-card-land">
           <img src={landscapeImage} alt="Landscape Project" className="project-image" />
-          <h3>SOCIUM: Finding Your Real Learning Partner</h3>
+          <h3>AQSA Official Website: Crafting an Elegant Online Presence</h3>
           <p className="project-description">
-          Socium is a web app that connects people with similar interests to find learning partners. It helps users learn and share skills together. You create a profile, and select a matching compatible learning partner. You can chat securely and set up learning sessions based on your schedules. Socium fosters motivation and accountability, making learning enjoyable and effective. It also helps build friendships and professional networks within the learning community.
+          The official website for <a className='p-link' href='https://www.alqamaraqsa.online'>AQSA</a> Student Association has been meticulously designed and developed, ensuring a seamless and responsive user experience. This elegant and modern platform showcases AQSA's student-centric initiatives. Focused on creating a visually appealing interface that adapts to various devices, the site serves as a hub for students to explore AQSA's offerings and reflects the association's commitment to professionalism and excellence. Explore this responsive website to witness a harmonious blend of design aesthetics and functionality.
           </p>
         </div>
       </div>
@@ -21,7 +21,7 @@ const ProjectSection = () => {
           <img src={portraitImage} alt="Portrait Project" className="project-image" />
           <h3>TripSpark: QR Based Bus Pass App</h3>
           <p className="project-description">
-          TripSpark is an innovative mobile app that ensures safe school bus journeys for students. With its QR code-based verification system, bus faculty can generate unique QR codes for each student, confirming their presence when they board the bus. Parents can track the school bus in real-time using the app, getting updates on its location and estimated arrival time, which brings them peace of mind. The app also offers a convenient payment feature for bus fare, making the entire transportation process more efficient and secure for everyone involved.
+          TripSpark features a crafted user interface for its innovative mobile app, dedicated to ensuring safe school bus journeys. The app utilizes a QR code-based system, allowing bus faculty to generate unique QR codes for each student, confirming their presence upon boarding. Parents can enjoy real-time tracking of the school bus, receiving updates on its location and estimated arrival time for peace of mind. Additionally, the app features a convenient payment option for bus fare, streamlining and securing the entire transportation process.
           </p>
         </div>
       </div>
